b1b8bfa4ce91d2f416746d2923220400ba29ab61
PaolaGreciaAalvarenga <122564449+PaolaGreciaAalvarenga@users.noreply.github.com>
2024-03-22 16:57:07 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,9 @@
+function stat(){
+    $("#início").hide();
+
+    $("#fundoGame").append("<div id= 'jogador' class= 'animal'></div>");
+    $("#fundoGame").append("<div id= 'inimigo1' class= 'animal'></div>");
+    $("#fundoGame").append("<div id= 'inimigo2' class= 'animal'></div>");
+    $("#fundoGame").append("<div id= 'amigo' class= 'animal'></div>");
+    
+}
